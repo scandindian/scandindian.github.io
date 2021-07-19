45b9471f84b00e1a7f7440b024b7fe4529a38198
Varun Kumar <heysadboy@gmail.com>
2021-07-19 23:41:17 +0900

    updating website

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -7,14 +7,12 @@ const NavigationBar = () => {
             <div id="desktop-navigation-bar" className="ui top fixed text menu">
                 <a className="item">Home</a>
                 <div className="right menu">
-                    <a className="item">About</a>
                     <a className="item">Photos</a>
                     <a className="item">Contact</a>
                 </div>
             </div>
             <div id="mobile-navigation-bar" className="ui top fixed four item text menu">
                 <a className="item">Home</a>
-                <a className="item">About</a>
                 <a className="item">Photos</a>
                 <a className="item">Contact</a>
             </div>
--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -6,14 +6,16 @@ const Home = () => {
     return (
         <div id="home-container" className="ui grid middle aligned">
             <div className="row">
-                <div className="twelve wide column">
+                <div className="eleven wide column">
                     <div className="ui vertical segment">
                         <h3 className>Hello, my name is</h3>
                         <h1 className="name-heading">Varun Kumar</h1>
                         <p className="short-description">
-                            I am a full stack software developer with a focus on frontend with ReactJS and Redux. 
+                            I am a full stack software developer with a focus on frontend, currently based in Tokyo. 
                             <br />
-                            Currently, I am based in Tokyo.
+                            I specialize in developing apps using ReactJS and Redux along with other technologies.
+                            <br />
+                            See my résumé to know more about me.
                         </p>
                         <a id="resume-button" href={Resume} target="_blank" className="ui button">Résumé</a>
                     </div>
