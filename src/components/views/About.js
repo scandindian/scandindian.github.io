453c861a8f2a1644372106f3208ac96e5d126a5a
Varun Kumar <heysadboy@gmail.com>
2021-07-23 22:42:44 +0900

    Update About.js

--- src/components/views/About.js
+++ src/components/views/About.js
@@ -13,16 +13,16 @@ const About = () => {
                     Currently, I am working at <a href="https://www.jig-saw.com/en/" target="_blank" rel="noopener noreferrer">JIG-SAW</a> in Tokyo, Japan.
                     I moved here in 2019 after graduating with a masters degree in information technology from <a href="https://iiita.ac.in/" target="_blank" rel="noopener noreferrer">IIIT Allahabad</a>.
                     <br /><br />
-                    Before moving to Japan I have worked
+                    Before moving to Japan I have worked as an intern
                     at <a href="https://www.itri.org.tw/english/" target="_blank" rel="noopener noreferrer">ITRI</a> in Hsinchu, Taiwan
-                    and <a href="https://www.gist.ac.kr/en/main.html" target="_blank" rel="noopener noreferrer">GIST</a> in Gwangju, South Korea as an intern during college.
+                    and <a href="https://www.gist.ac.kr/en/main.html" target="_blank" rel="noopener noreferrer">GIST</a> in Gwangju, South Korea during college.
                     <br /><br />
-                    My current passion is working with ReactJS and Redux for the frontend. For the backend, I use the Django REST framework and AWS.
+                    My passion is developing frontend with ReactJS and Redux. For the backend, I use the Django REST framework and AWS.
                     I have also worked with <a href="https://play.google.com/store/apps/developer?id=heysadboy&hl=en" target="_blank" rel="noopener noreferrer">Android</a> in the past while working as an open source developer in <a href="https://summerofcode.withgoogle.com/archive/2017/projects/5117290592862208/" target="_blank" rel="noopener noreferrer">Google Summer of Code with FOSSASIA</a>.
                     <br /><br />
-                    In the future, I would like to keep working as a frontend developer. I love developing beautiful and scalable web apps with a focus on user experience.
+                    In the future, I would like to keep working as a frontend developer. I love to design, develop and deploy beautiful and scalable web apps with a focus on user experience.
                     <br /><br />
-                    Feel free to <Link to="/contact">connect</Link> with me or see me <a id="resume-button" href="/Resume.pdf" target="_blank" rel="noopener noreferrer">résumé</a>. I am looking forward to hearing from you.
+                    Feel free to <Link to="/contact">connect</Link> with me or see my <a id="resume-button" href="/Resume.pdf" target="_blank" rel="noopener noreferrer">résumé</a>. I am looking forward to hearing from you.
                     </p>
                 </div>
             </div>
