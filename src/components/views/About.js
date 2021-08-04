74c5d5b7505cf391d0e13c149d4724bffeb2a7c6
Varun Kumar <heysadboy@gmail.com>
2021-08-04 13:33:12 +0900

    Update About.js

--- src/components/views/About.js
+++ src/components/views/About.js
@@ -16,7 +16,7 @@ const About = () => {
                                 During my college summer vacations, I worked
                                 at <a href="https://www.itri.org.tw/english/" target="_blank" rel="noopener noreferrer">ITRI</a> in Taiwan and <a href="https://www.gist.ac.kr/en/main.html" target="_blank" rel="noopener noreferrer">GIST</a> in South Korea as an intern.
                                 <br /><br />
-                                My passion is frontend development with ReactJS and Redux. For the backend, I use Python and AWS.
+                                My passion is frontend development with ReactJS and Redux. For the backend, I use AWS and Python.
                                 I have also developed <a href="https://play.google.com/store/apps/developer?id=heysadboy&hl=en" target="_blank" rel="noopener noreferrer">android apps</a> and worked as an open source developer in <a href="https://summerofcode.withgoogle.com/archive/2017/projects/5117290592862208/" target="_blank" rel="noopener noreferrer">Google Summer of Code with FOSSASIA</a>.
                                 <br /><br />
                                 In the future, I would like to keep working as a frontend developer. I love to design, develop and deploy beautiful and scalable web apps with a focus on user experience.
